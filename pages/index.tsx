1fa16164724bddcdefbb01001acc26ea4560afd1
HiepDo <bacodekiller@gmail.com>
2021-12-19 15:10:22 +0700

    feat: init project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import ButtonSubmit from '../components/Button'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
+    <h1 className="text-lime-400 text-3xl">Hello Next.js 👋</h1>
+
+    <p className="mr-2">The Button with Ant-Design</p>
+    <ButtonSubmit />
+
     <p>
       <Link href="/about">
         <a>About</a>
